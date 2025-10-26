88b145e37137963c7b332b25e8388c78bb5673fd
Puritized <pecamoint@gmail.com>
2025-10-26 07:24:32 +0100

    W3RA

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '',
+  base: '/react-js-jsx-and-css-mastering-front-end-development-Puritized',
 });
+
